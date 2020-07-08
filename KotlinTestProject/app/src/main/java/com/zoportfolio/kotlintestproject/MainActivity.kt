d60024268ba6e80ffc3db1ab1714208643f96d90
Zakarie Ortiz <ZOrtiz42@outlook.com>
2020-07-08 12:41:44 -0400

    ADD
    - Added TODO in the main activity.

--- KotlinTestProject/app/src/main/java/com/zoportfolio/kotlintestproject/MainActivity.kt
+++ KotlinTestProject/app/src/main/java/com/zoportfolio/kotlintestproject/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO: For this test project I want to simple do the following,
+        // Grab on stocks data from the alpaca api using retrofit
+        // display that data in a spark view from the robinhood library, and a robinhood ticker,
+        // use dependency injection to make the code more suitable and readable,
+        // and finally, use the MVVM Architecture as a baseline.
     }
 }
